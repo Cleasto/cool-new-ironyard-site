f744e51a97360da3d0b763027367984941555c57
deploy.sh <joetamburro38@gmail.com>
2015-02-09 16:37:17 -0500

    removed old script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -86,13 +86,6 @@ function chatWindowColor(){
   }
 }
 
-function wufooTags (campus, course , scholarship){
-  $('#form4').submit(function(){
-    string = 'Campus: ' + campus + ',Class: ' + course + ',' + scholarship
-    console.log(string)
-  })
-}
-
 $(document).ready(function(){
   playVideo();
   toggleChat();
